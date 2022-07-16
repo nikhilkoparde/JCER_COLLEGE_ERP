4c9e6fcf9f361000b5949b255a6a033b9da6193b
nikhilkoparde <nikhilkoparde8123@gmail.com>
2022-07-16 20:35:55 +0530

    first commit

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,3 +139,4 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
 }
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
